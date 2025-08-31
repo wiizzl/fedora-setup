0605368a75d85d459f3c7f58b7060be942bf500f
wiizzl <github@houlliere.com>
2025-08-31 13:41:03 +0200

    Fix syntax error in packages.sh by adding missing quotes and ensuring proper formatting

--- modules/packages.sh
+++ modules/packages.sh
@@ -4,5 +4,5 @@ echo "Installing additional packages..."
 
 sudo dnf install -y $(cat "$(dirname "$0")/../lists/add-packages.txt")
 
-echo "Removing unwanted packages...
-sudo dnf remove -y $(cat "$(dirname "$0")/../lists/remove-packages.txt")"
+echo "Removing unwanted packages..."
+sudo dnf remove -y $(cat "$(dirname "$0")/../lists/remove-packages.txt")
